378ef9100063bad542d8de080d90a8ad03739bc2
olegsvs <oleg.texet@gmail.com>
2024-09-10 11:38:10 +0300

    Server availability during rebuild

--- restart.sh
+++ restart.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/sh
 ps -ef | grep 'serve -s' | awk '{print $2}'  | xargs -r kill -9
 sleep 2
-nohup serve -s build > frontend.log &
+rm -rf prod_build
+cp -rf build prod_build
+nohup serve -s prod_build > frontend.log &
