a83167b55fe500fdc30cda33b5090cd40c07258d
olegsvs <oleg.texet@gmail.com>
2024-09-10 14:45:19 +0300

    Fix serve in background

--- restart.sh
+++ restart.sh
@@ -3,4 +3,4 @@ ps -ef | grep 'serve -s' | awk '{print $2}'  | xargs -r kill -9
 sleep 2
 rm -rf prod_build
 cp -rf build prod_build
-nohup serve -s prod_build > frontend.log &
+setsid nohup serve -s prod_build > frontend.log &
