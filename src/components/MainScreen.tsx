432d2d6517d7c32d34210b4fab230885e8bf5590
Maxim Velesyuk <max.velesyuk@gmail.com>
2024-09-12 23:24:09 +0200

    Add scroll restoration

--- src/components/MainScreen.tsx
+++ src/components/MainScreen.tsx
@@ -1,17 +1,19 @@
-import { Box } from "@mui/material";
-import { Page } from "utils/types";
-import MainMenu from "./MainMenu";
+import { Box } from '@mui/material'
+import { ScrollRestoration } from 'react-router-dom'
+import { Page } from 'utils/types'
+import MainMenu from './MainMenu'
 
 type Props = {
-  currentPage: Page;
-  children?: React.ReactNode;
-};
+  currentPage: Page
+  children?: React.ReactNode
+}
 
 export default function MainScreen({ children, currentPage }: Props) {
   return (
     <Box>
       <MainMenu currentPage={currentPage} />
       {children}
+      <ScrollRestoration />
     </Box>
-  );
+  )
 }
