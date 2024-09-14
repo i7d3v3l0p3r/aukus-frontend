331006b45b761dc7c078d00f6cfdae7d9c33afbc
Maxim Velesyuk <max.velesyuk@gmail.com>
2024-09-14 10:24:15 +0200

    Add finish text

--- src/pages/map/components/CellItem.tsx
+++ src/pages/map/components/CellItem.tsx
@@ -1,31 +1,64 @@
-import { ArrowRightAltSharp } from "@mui/icons-material";
-import { Box, Chip, Typography } from "@mui/material";
-import { Player } from "utils/types";
-import { cellSize, MapCell } from "../types";
+import { ArrowRightAltSharp } from '@mui/icons-material'
+import { Box, Chip, Typography } from '@mui/material'
+import { Player } from 'utils/types'
+import { cellSize, MapCell } from '../types'
 
 type Props = {
-  cell: MapCell;
-  currentPlayer?: Player;
-  moveSteps?: number;
-};
+  cell: MapCell
+  currentPlayer?: Player
+  moveSteps?: number
+}
 
 export default function CellItem({ cell, currentPlayer, moveSteps }: Props) {
   const directionIcons = {
-    right: <ArrowRightAltSharp sx={{ fontSize: 30, verticalAlign: "middle" }} />,
-    left: <ArrowRightAltSharp sx={{ fontSize: 30, verticalAlign: "middle", transform: "rotate(180deg)" }} />,
-    up: <ArrowRightAltSharp sx={{ fontSize: 30, verticalAlign: "middle", transform: "rotate(-90deg)" }} />,
-  };
+    right: (
+      <ArrowRightAltSharp sx={{ fontSize: 30, verticalAlign: 'middle' }} />
+    ),
+    left: (
+      <ArrowRightAltSharp
+        sx={{
+          fontSize: 30,
+          verticalAlign: 'middle',
+          transform: 'rotate(180deg)',
+        }}
+      />
+    ),
+    up: (
+      <ArrowRightAltSharp
+        sx={{
+          fontSize: 30,
+          verticalAlign: 'middle',
+          transform: 'rotate(-90deg)',
+        }}
+      />
+    ),
+  }
 
-  const label = cell.id === 0 ? "Старт" : cell.id === 101 ? "Финиш" : cell.id;
+  let label = `${cell.id}`
+  if (cell.id === 0) {
+    label = 'Старт'
+  } else if (cell.id === 102) {
+    label = 'Финиш'
+  } else if (cell.id === 101) {
+    label = 'Рывок'
+  }
 
-  let relativeLocation = null;
+  let relativeLocation = null
   if (currentPlayer && moveSteps && moveSteps !== 0) {
-    const target = currentPlayer.map_position + moveSteps;
-    if (moveSteps > 0 && cell.id > currentPlayer.map_position && cell.id <= target) {
-      relativeLocation = cell.id - currentPlayer.map_position;
+    const target = currentPlayer.map_position + moveSteps
+    if (
+      moveSteps > 0 &&
+      cell.id > currentPlayer.map_position &&
+      cell.id <= target
+    ) {
+      relativeLocation = cell.id - currentPlayer.map_position
     }
-    if (moveSteps < 0 && cell.id < currentPlayer.map_position && cell.id >= target) {
-      relativeLocation = cell.id - currentPlayer.map_position;
+    if (
+      moveSteps < 0 &&
+      cell.id < currentPlayer.map_position &&
+      cell.id >= target
+    ) {
+      relativeLocation = cell.id - currentPlayer.map_position
     }
   }
 
@@ -35,10 +68,10 @@ export default function CellItem({ cell, currentPlayer, moveSteps }: Props) {
       style={{
         minHeight: cellSize,
         minWidth: cellSize,
-        position: "relative",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
+        position: 'relative',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
       }}
     >
       <Box alignContent="center" position="absolute" top={0} left={0}>
@@ -46,12 +79,16 @@ export default function CellItem({ cell, currentPlayer, moveSteps }: Props) {
         {cell.direction && directionIcons[cell.direction]}
       </Box>
       {relativeLocation && (
-        <Box textAlign={"center"}>
-          <Typography variant="h3" fontWeight={100} color={relativeLocation > 0 ? "#546e7a" : "#5d4037"}>
+        <Box textAlign={'center'}>
+          <Typography
+            variant="h3"
+            fontWeight={100}
+            color={relativeLocation > 0 ? '#546e7a' : '#5d4037'}
+          >
             {Math.abs(relativeLocation)}
           </Typography>
         </Box>
       )}
     </div>
-  );
+  )
 }
--- src/pages/map/components/MapComponent.tsx
+++ src/pages/map/components/MapComponent.tsx
@@ -1,4 +1,4 @@
-import { Box, Grid } from '@mui/material'
+import { Box, Grid, Typography } from '@mui/material'
 import { NextTurnParams, Player } from 'utils/types'
 import { useState, Fragment } from 'react'
 import { cellSize, MainMap, MapCell } from '../types'
@@ -8,10 +8,12 @@ import PlayerIcon from './PlayerIcon'
 import {
   ladders,
   laddersByCell,
+  lastCell,
   mapCellRows,
   mapCellsSorted,
   snakes,
   snakesByCell,
+  startCell,
 } from './utils'
 import { createPlayerMove, fetchPlayers } from 'utils/api'
 import { useMutation, useQuery } from '@tanstack/react-query'
@@ -22,9 +24,6 @@ import SVGMarkers from './SVGMarkers'
 import TesterButton from './TesterButton'
 
 export default function MapComponent() {
-  const finishCell = { id: 101, direction: null } as MapCell
-  const startCell = { id: 0, direction: 'right' } as MapCell
-
   const [closePopups, setClosePopups] = useState(false)
   const [moveSteps, setMoveSteps] = useState(0)
 
@@ -50,7 +49,7 @@ export default function MapComponent() {
     cellRows: mapCellRows,
     cells: mapCellsSorted,
     startCell,
-    finishCell,
+    finishCell: lastCell,
   }
 
   const handleClick = () => {
@@ -118,6 +117,17 @@ export default function MapComponent() {
       onClick={handleClick}
     >
       <SVGMarkers />
+
+      <Grid container justifyContent="center" marginBottom={2} columns={12}>
+        <Grid item xs={1}>
+          <Box width={cellSize} />
+        </Grid>
+        <Grid item xs={11}>
+          <Typography variant="h4">ФИНИШ</Typography>
+          <Box width={cellSize * 11} />
+        </Grid>
+      </Grid>
+
       <Grid
         container
         justifyContent={'center'}
@@ -128,10 +138,10 @@ export default function MapComponent() {
         }}
       >
         {map.cellRows.map((row, index) => (
-          <Grid container item key={index} xs="auto">
+          <Grid container key={index} xs="auto">
             {index === 0 && (
               <Grid item borderTop={1} borderRight={1} borderLeft={1}>
-                <CellItem cell={finishCell} />
+                <CellItem cell={lastCell} />
               </Grid>
             )}
             {index === 9 && (
