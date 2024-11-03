c3366cf75886f0168f0c7dcf5e8d0344753cb957
Maxim Velesyuk <max.velesyuk@gmail.com>
2024-11-03 14:05:33 +0100

    Make arrows smaller and transparent

--- src/pages/map/components/SVGMarkers.tsx
+++ src/pages/map/components/SVGMarkers.tsx
@@ -1,50 +1,58 @@
 export default function SVGMarkers() {
+
+  const baseColor = 'rgba(0,0,0,0.2)'
+  const greenColor = 'rgba(0,255,0,0.5)'
+  const redColor = 'rgba(255,0,0,0.5)'
+
+  const arrowPoints = '0 0, 6 2, 0 4'
+
   return (
     <svg style={{ height: 0, display: 'block' }}>
       <defs>
         <marker
           id="green-arrow-start"
-          markerWidth="10"
-          markerHeight="7"
-          refX="-10"
-          refY="3.5"
+          markerWidth="6"
+          markerHeight="4"
+          refX="-6"
+          refY="2"
           orient="auto"
-          stroke="white"
+          stroke={baseColor}
+          strokeWidth={1}
         >
-          <polygon points="0 0, 10 3.5, 0 7" fill="green" />
+          <polygon points={arrowPoints} fill={greenColor} />
         </marker>
         <marker
           id="green-arrow-end"
-          markerWidth="10"
-          markerHeight="7"
+          markerWidth="6"
+          markerHeight="4"
           refX="0"
-          refY="3.5"
+          refY="2"
           orient="auto"
-          stroke="white"
+          stroke={baseColor}
         >
-          <polygon points="0 0, 10 3.5, 0 7" fill="green" />
+          <polygon points={arrowPoints} fill={greenColor} />
         </marker>
         <marker
           id="red-arrow-start"
-          markerWidth="10"
-          markerHeight="7"
+          markerWidth="6"
+          markerHeight="4"
           refX="-10"
-          refY="3.5"
+          refY="2"
           orient="auto"
-          stroke="white"
+          stroke={baseColor}
         >
-          <polygon points="0 0, 10 3.5, 0 7" fill="red" />
+          <polygon points={arrowPoints} fill={redColor} />
         </marker>
         <marker
           id="red-arrow-end"
-          markerWidth="10"
-          markerHeight="7"
+          markerWidth="6"
+          markerHeight="4"
           refX="0"
-          refY="3.5"
+          refY="2"
           orient="auto"
-          stroke="white"
+          stroke={baseColor}
         >
-          <polygon points="0 0, 10 3.5, 0 7" fill="red" />
+          <polygon points={arrowPoints} fill={redColor} />
         </marker>
       </defs>
     </svg>
