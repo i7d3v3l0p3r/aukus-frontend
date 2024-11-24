eb75154128a5a4074799aaf2544d2f8a047948b8
Maxim Velesyuk <max.velesyuk@gmail.com>
2024-11-24 15:47:23 +0100

    Add game autocomplete to vod edit modal

--- src/pages/player/components/EditVodModal.tsx
+++ src/pages/player/components/EditVodModal.tsx
@@ -1,5 +1,6 @@
 import { Close } from '@mui/icons-material'
 import {
+  Autocomplete,
   Box,
   Button,
   Dialog,
@@ -9,7 +10,11 @@ import {
   IconButton,
   TextField,
 } from '@mui/material'
-import { useState } from 'react'
+import { useQuery } from '@tanstack/react-query'
+import { ChangeEvent, useEffect, useState } from 'react'
+import ImagePlaceholder from 'assets/icons/image_placeholder.svg?react'
+import { fetchGameNames } from 'src/utils/api'
+import { CustomPopper } from 'src/pages/map/components/action/TurnModal'
 
 type Props = {
   open: boolean
@@ -28,6 +33,42 @@ export default function EditVodModal({
 }: Props) {
   const [vodValue, setVodValue] = useState(vodText)
   const [itemTitle, setItemTitle] = useState(title)
+
+  const {
+    data: gameNamesData,
+    dataUpdatedAt: updateTs,
+    refetch,
+    fetchStatus,
+    status,
+  } = useQuery({
+    queryKey: ['game_names'],
+    queryFn: () => fetchGameNames(itemTitle),
+    enabled: itemTitle.length > 3,
+  })
+
+  useEffect(() => {
+    if (
+      status !== 'pending' &&
+      fetchStatus === 'idle' &&
+      itemTitle.length > 3 &&
+      updateTs + 500 < Date.now()
+    ) {
+      refetch()
+    }
+  }, [itemTitle.length, updateTs, refetch, fetchStatus])
+
+  let titleOptions: string[] = []
+  let imageUrl: string | null = null
+
+  if (itemTitle.length > 3 && gameNamesData && gameNamesData.games.length > 0) {
+    titleOptions = gameNamesData.games.map((game) => game.gameName)
+
+    const matchingUrl =
+      gameNamesData.games.find((game) => game.gameName === itemTitle)
+        ?.box_art_url || gameNamesData.games[0].box_art_url
+
+    imageUrl = matchingUrl.replace('{width}', '200').replace('{height}', '300')
+  }
 
   const handleVodChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setVodValue(e.target.value)
@@ -71,24 +112,57 @@ export default function EditVodModal({
           paddingBottom: '50px',
         }}
       >
-        <Box marginBottom="10px">Обновить название игры</Box>
-        <TextField
-          fullWidth
-          rows={1}
-          value={itemTitle}
-          onChange={(e) => setItemTitle(e.target.value)}
-          InputProps={{
-            style: {
-              paddingTop: '10px',
-              paddingLeft: '15px',
-              paddingRight: '15px',
-              paddingBottom: '10px',
-              lineHeight: '1.2',
-              fontSize: '16px',
-              fontWeight: 500,
-            },
-          }}
-        />
+        <Box display="flex">
+          <Box marginRight={'10px'}>
+            {imageUrl ? (
+              <img
+                src={imageUrl}
+                alt="game"
+                style={{
+                  width: '120px',
+                  height: '180px',
+                  borderRadius: '10px',
+                }}
+              />
+            ) : (
+              <ImagePlaceholder
+                width={'120px'}
+                height={'180px'}
+                style={{ borderRadius: '10px' }}
+              />
+            )}
+          </Box>
+          <Box>
+            <Box marginBottom="10px">Обновить название игры</Box>
+            <Autocomplete
+              freeSolo
+              fullWidth
+              PopperComponent={CustomPopper}
+              options={titleOptions}
+              value={itemTitle}
+              onChange={(_, newValue) => {
+                setItemTitle(newValue || '')
+              }}
+              renderInput={(params) => (
+                <TextField
+                  onChange={(
+                    event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+                  ) => setItemTitle(event.target.value)}
+                  {...params}
+                  style={{
+                    paddingTop: '10px',
+                    paddingBottom: '10px',
+                    fontSize: '16px important!',
+                  }}
+                />
+              )}
+              sx={{
+                width: '420px',
+              }}
+              className={titleOptions.length > 0 ? 'has-options' : 'no-options'}
+            />
+          </Box>
+        </Box>
         <Box marginTop={'20px'} marginBottom="10px">
           Обновить ссылки на записи стримов
         </Box>
