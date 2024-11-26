04cce67df7150256923df19611fedf20bf7b881d
Maxim Velesyuk <max.velesyuk@gmail.com>
2024-11-26 16:08:40 +0100

    Update 404

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -4,7 +4,10 @@ export default function NotFound() {
   return (
     <Box display="flex" justifyContent="center">
       <Box marginTop={'150px'}>
-        <img src="https://cdn.7tv.app/emote/01HNKT4960000EXZQWSVKBCAGF/4x.png" />
+        <img
+          src="https://cdn.7tv.app/emote/01J4RNW84G0006H6JAMR0FAC09/2x.avif"
+          width={'300px'}
+        />
         <Box textAlign={'center'} marginTop={'20px'} fontSize={'32px'}>
           404 Не Найдено
         </Box>
