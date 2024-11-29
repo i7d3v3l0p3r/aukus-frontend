76ca25e71bf19ed7ac1cad39954b9fce0c60d382
Maxim Velesyuk <max.velesyuk@gmail.com>
2024-11-29 22:56:05 +0100

    Update difficulty wheel

--- src/pages/rules/components/DifficultyWheel.tsx
+++ src/pages/rules/components/DifficultyWheel.tsx
@@ -2,14 +2,19 @@ import { Box } from '@mui/material'
 
 export default function DifficultyWheel() {
   return (
-    <Box style={{ transform: 'scale(0.8)' }}>
-      <iframe
-        src="https://wheelofnames.com/ru/krm-bsb"
-        title="Колесо сложности"
-        width={'700px'}
-        height={'500px'}
-        style={{ backgroundColor: 'black', border: 'none' }}
-      />
+    <Box display={'flex'} justifyContent={'center'}>
+      <Box style={{ backgroundColor: 'black', width: 'fit-content' }}>
+        <Box textAlign={'center'} fontSize={'32px'}>
+          Ролл сложности игры
+        </Box>
+        <iframe
+          src="https://wheelofnames.com/ru/krm-bsb"
+          title="Колесо сложности"
+          width={'700px'}
+          height={'500px'}
+          style={{ border: 'none' }}
+        />
+      </Box>
     </Box>
   )
 }
