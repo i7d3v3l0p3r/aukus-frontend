d086aee7def1d39bb37715fb702fe0f711ebb5f2
Maxim Velesyuk <max.velesyuk@gmail.com>
2024-12-02 16:06:15 +0100

    Update rules

--- src/pages/rules/RulesPage.tsx
+++ src/pages/rules/RulesPage.tsx
@@ -2,8 +2,6 @@ import { Box } from '@mui/material'
 import BottomSection from 'components/BottomSection'
 import MainMenu from 'components/MainMenu'
 import RulesContainer from './components/RulesConainer'
-import DifficultyWheel from './components/DifficultyWheel'
-import DifficultyButton from './components/DifficultyButton'
 
 export default function RulesPage() {
   return (
